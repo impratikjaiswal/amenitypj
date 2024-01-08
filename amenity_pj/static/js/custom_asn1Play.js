84435331e4a08b53e118566c4b40343409daa80f
imPratik <impratikjaiswal@gmail.com>
2024-01-08 20:32:09 +0530

    v4.2.0

--- amenity_pj/static/js/custom_asn1Play.js
+++ amenity_pj/static/js/custom_asn1Play.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
     // alert("DOM is ready");
-    $('#asn1_schemas').change(function () {
-        // alert("asn1_schemas has been changed.");
+    $('#asn1_schema').change(function () {
+        // alert("asn1_schema has been changed.");
         $.getJSON('/asn1Play/asn1Objects', {
-            selected_asn1_schema_js: $('#asn1_schemas').val()
+            selected_asn1_schema_js: $('#asn1_schema').val()
         }).done(function (data) {
             // let msg = "";
             // msg = data.html_string_selected;
             // document.getElementById("debug_data").innerHTML += msg;
-            $('#asn1_objects').html(data.html_string_selected);
+            $('#asn1_object').html(data.html_string_selected);
         })
     });
     $('#process_input').bind('click', function () {
         $.getJSON('/_process_data', {
-            selected_asn1_schema_js: $('#asn1_schemas').val(),
-            selected_asn1_object_js: $('#asn1_objects').val(),
+            selected_asn1_schema_js: $('#asn1_schema').val(),
+            selected_asn1_object_js: $('#asn1_object').val(),
         }).success(function (data) {
             $('#debug_data').text(data.random_text);
         })
