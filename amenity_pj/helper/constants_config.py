bd500048ee50107652e4b8c0514143e4eebc38b7
imPratik <impratikjaiswal@gmail.com>
2024-04-15 09:12:30 +0530

    v4.2.5.1

--- amenity_pj/helper/constants_config.py
+++ amenity_pj/helper/constants_config.py
@@ -1,5 +1,5 @@
 class ConfigConst:
-    TOOL_VERSION = '4.2.5'
+    TOOL_VERSION = '4.2.5.1'
     TOOL_VERSION_DETAILED = f'v{TOOL_VERSION}'
     TOOL_NAME = 'AmenityPj'
     TOOL_TITLE = 'Amenity Pj'
