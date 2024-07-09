fda50cdbe008761b5d12bd1a53d25794ec1c6c97
imPratik <impratikjaiswal@gmail.com>
2024-07-09 10:08:16 +0530

    2nd nav bar is changed to sidebar; fixed css property removed from navbar

--- amenity_pj/_version.py
+++ amenity_pj/_version.py
@@ -7,5 +7,5 @@ Provides amenity_pj version information.
 
 from incremental import Version
 
-__version__ = Version("amenity_pj", 6, 4, 1)
+__version__ = Version("amenity_pj", 6, 4, 2)
 __all__ = ["__version__"]
