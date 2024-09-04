f1214f62bece5969759fd9fbe1bfd7b3c33a4e04
imPratik <impratikjaiswal@gmail.com>
2024-09-04 10:10:46 +0530

    line numbers added in output

--- amenity_pj/static/js/formatted/apj.js
+++ amenity_pj/static/js/formatted/apj.js
@@ -423,6 +423,9 @@ function setText(event) {
 function inputDataLines() {
     const textarea = document.getElementById("input_data");
     const lineNumbersEle = document.getElementById('input_data_line_numbers');
+    if (textarea == null) {
+        return
+    }
     const textareaStyles = window.getComputedStyle(textarea, null);
     [
         'fontFamily',
@@ -513,97 +516,19 @@ function inputDataLines() {
     });
 }
 
-
 function outputDataLines() {
     const textarea = document.getElementById('output_statement');
     const lineNumbersEle = document.getElementById('output_statement_line_numbers');
-    const textareaStyles = window.getComputedStyle(textarea);
-    [
-        'fontFamily',
-        'fontSize',
-        'fontWeight',
-        'letterSpacing',
-        'lineHeight',
-        'padding',
-    ].forEach((property) => {
-        lineNumbersEle.style[property] = textareaStyles[property];
-    });
-
-    const parseValue = (v) => v.endsWith('px') ? parseInt(v.slice(0, -2), 10) : 0;
-    const font = `${textareaStyles.fontSize} ${textareaStyles.fontFamily}`;
-    const paddingLeft = parseValue(textareaStyles.paddingLeft);
-    const paddingRight = parseValue(textareaStyles.paddingRight);
-
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    context.font = font;
-
-    const calculateNumLines = (str) => {
-        const textareaWidth = textarea.getBoundingClientRect().width - paddingLeft - paddingRight;
-        const words = str.split(' ');
-        let lineCount = 0;
-        let currentLine = '';
-        for (let i = 0; i < words.length; i++) {
-            const wordWidth = context.measureText(words[i] + ' ').width;
-            const lineWidth = context.measureText(currentLine).width;
-
-            if (lineWidth + wordWidth > textareaWidth) {
-                lineCount++;
-                currentLine = words[i] + ' ';
-            } else {
-                currentLine += words[i] + ' ';
-            }
-        }
-
-        if (currentLine.trim() !== '') {
-            lineCount++;
-        }
-
-        return lineCount;
-    };
-
-    const calculateLineNumbers = () => {
-        // ID of the object
-        const lines = textarea.innerText.split('\n');
-        const numLines = lines.map((line) => calculateNumLines(line));
-
-        let lineNumbers = [];
-        let i = 1;
-        while (numLines.length > 0) {
-            const numLinesOfSentence = numLines.shift();
-            lineNumbers.push(i);
-            if (numLinesOfSentence > 1) {
-                Array(numLinesOfSentence - 1)
-                    .fill('')
-                    .forEach((_) => lineNumbers.push(''));
-            }
-            i++;
-        }
-
-        return lineNumbers;
-    };
-
+    if (textarea == null) {
+        return
+    }
     const displayLineNumbers = () => {
-        const lineNumbers = calculateLineNumbers();
-        lineNumbersEle.innerHTML = Array.from({
-            length: lineNumbers.length
-        }, (_, i) => `<div>${lineNumbers[i] || '&nbsp;'}</div>`).join('');
-    };
-
-    textarea.addEventListener('input', () => {
-        displayLineNumbers();
-    });
-
+        const lines = textarea.innerText.split('\n');
+        let lines_data = Array.from({
+            length: lines.length,
+        }, (_, i) => `${i + 1}`).join('\n');
+        debugData(lines_data, heading = "lines_data")
+        lineNumbersEle.innerHTML = "<pre>" + lines_data + "<pre>";
+    }
     displayLineNumbers();
-
-    const ro = new ResizeObserver(() => {
-        const rect = textarea.getBoundingClientRect();
-        lineNumbersEle.style.height = `${rect.height}px`;
-        displayLineNumbers();
-    });
-    ro.observe(textarea);
-
-    textarea.addEventListener('scroll', () => {
-        lineNumbersEle.scrollTop = textarea.scrollTop;
-    });
 }
