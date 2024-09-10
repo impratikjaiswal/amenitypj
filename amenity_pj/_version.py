dedbaa45cd99f89ecbd42600368320e3edd4208e
imPratik <impratikjaiswal@gmail.com>
2024-09-10 09:55:06 +0530

    suprise me feature freezed

--- amenity_pj/_version.py
+++ amenity_pj/_version.py
@@ -7,5 +7,5 @@ Provides amenity_pj version information.
 
 from incremental import Version
 
-__version__ = Version("amenity_pj", 6, 9, 16)
+__version__ = Version("amenity_pj", 6, 9, 17)
 __all__ = ["__version__"]
